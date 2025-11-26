a9d033792ef25bc18afe2d7b6bf752e9457fa07e
Carson Holscher <firefoxsalesman2@protonmail.com>
2025-11-26 10:59:00 -0500

    emacs-overlay is no longer in nix flake jail

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
-    emacs-overlay.url  = "github:nix-community/emacs-overlay/c5ba5a5";
+    emacs-overlay.url  = "github:nix-community/emacs-overlay";
     
     stylix.url = "github:danth/stylix";
     
